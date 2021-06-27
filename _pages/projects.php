4c378401ac36c12f8b7bad200354ba598058db60
Abhishekgarg97 <59972803+Abhishekgarg97@users.noreply.github.com>
2021-06-27 16:01:56 +0530

    dynamic authentication

---
+++ _pages/projects.php
@@ -0,0 +1,80 @@
+<div class="container">
+      <div class="row">
+        <div class="col-md-4">
+          <div class="section-title">
+            <h1>Projects</h1>
+          </div>
+        </div>
+        <div class="col-md-8">
+          <div class="text-info">
+            <h4>My Recent Work</h4>
+          </div>
+          <div class="portfolio-content">
+            <div class="row">
+              <div class="col-sm-6">
+                <div class="portfolio-item">
+                  <img class="img-fluid" src="images/studmgtsystm/studreg.jpg" alt="Image">
+                  <div class="portfolio-overlay">
+                    <div class="portfolio-info">
+                      <a href="images/studmgtsystm/login.jpg"><i class="fa fa-camera-retro" aria-hidden="true"></i></a>
+                      <h3>Student Management System</h3>
+                      <p>Java Project</p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div class="col-sm-6">
+                <div class="portfolio-item">
+                  <img class="img-fluid" src="images/brickbraker/1.jpg" alt="Image">
+                  <div class="portfolio-overlay">
+                    <div class="portfolio-info">
+                      <a href="images/brickbraker/2.jpg"><i class="fa fa-camera-retro" aria-hidden="true"></i></a>
+                      <h3>Brick Braker</h3>
+                      <p>Java project</p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div class="col-sm-6">
+                <div class="portfolio-item">
+                  <img class="img-fluid" src="images/jobscope/home.jpg" alt="Image">
+                  <div class="portfolio-overlay">
+                    <div class="portfolio-info">
+                      <a href="images/jobscope/register.jpg"><i class="fa fa-camera-retro" aria-hidden="true"></i></a>
+                      <h3>JobScope</h3>
+                      <p>PHP project</p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <!-- new projects -->
+              <div class="col-sm-6">
+                <div class="portfolio-item">
+                  <img class="img-fluid" src="images/projects/resurantwebsite/21cmenu.png" alt="Image">
+                  <div class="portfolio-overlay">
+                    <div class="portfolio-info">
+                      <a href="images/projects//resurantwebsite/fullimg.png"><i class="fa fa-camera-retro" aria-hidden="true"></i></a>
+                      <h3>Resturant website</h3>
+                      <p>HTML,CSS</p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div class="col-sm-6">
+                <div class="portfolio-item">
+                  <img class="img-fluid" src="images/projects/androidRecyvlerview/outputscreen.jpeg" alt="Image">
+                  <div class="portfolio-overlay">
+                    <div class="portfolio-info">
+                      <a href="images/projects/androidRecyvlerview/editwindow.jpeg"><i class="fa fa-camera-retro" aria-hidden="true"></i></a>
+                      <h3>Student Name and Marks using RecylerView</h3>
+                      <p>Android Kotlin</p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  
---
+++ _pages/resume.php
@@ -0,0 +1,250 @@
+<div class="container">
+      <div class="row">
+        <div class="col-md-4">
+          <div class="section-title">
+            <h1>My Resume</h1>
+          </div>
+        </div>
+        <div class="col-md-8">
+          <div class="section-content">
+            <div class="profile-logo">
+              <img class="img-fluid" src="images/myprofile.jpg" alt="Image">
+            </div>
+            <div class="profile-info">
+              <h1>Abhishek Garg</h1>
+              <h4>Fresher</h4>
+              <address>
+                <p>Address: SB-22 SHASTRI NAGAR,GHAZIABAD 201002<br> Phone: +91-7599279317 , +91-95570181256 <br> <a
+                    href="#">Email: <span> abhishekgarg1218@gmail.com</span> </span></a></p>
+              </address>
+            </div>
+            <div class="career-objective">
+              <div class="title">
+                <div class="icons">
+                  <i class="fa fa-black-tie" aria-hidden="true"></i>
+                </div>
+                <h3>Career Objective</h3>
+              </div>
+              <div class="sub-content">
+                <p>Hard-working individual with proficiency in HTML, JavaScript, and CSS, as well as ability to
+                  communicate effectively in a team setting, and possessing a strong willingness to learn about
+                  entrepreneurship and business seeks to apply for the position of Intern Front End Developer</p>
+              </div>
+            </div>
+            <hr>
+            <div class="work-history">
+              <div class="title">
+                <div class="icons">
+                  <i class="fa fa-briefcase" aria-hidden="true"></i>
+                </div>
+                <h3>Work History</h3>
+              </div>
+              <div class="sub-content">
+                <div class="history">
+                  <h5>Web Devloper</h5>
+                  <h6>2019(3 Months)</h6>
+                  <p></p>
+                </div>
+              </div>
+            </div>
+            <hr>
+            <div class="educational-background">
+              <div class="title">
+                <div class="icons">
+                  <i class="fa fa-graduation-cap" aria-hidden="true"></i>
+                </div>
+                <h3>Educational Background</h3>
+              </div>
+              <div class="sub-content">
+                <div class="education">
+                  <h5>Master of Computer Application @ Manipal Institute of Technology</h5>
+                  <p>july 2019 to june 2021</p>
+                </div>
+                <hr>
+                <div class="education">
+                  <h5>Bachelor of Computer Applications @ Sunderdeep College of Management Technology</h5>
+                  <p>july 2015 to june 2018</p>
+                </div>
+              </div>
+            </div>
+            <hr>
+            <div class="language-proficiency">
+              <div class="title">
+                <div class="icons">
+                  <i class="fa fa-language" aria-hidden="true"></i>
+                </div>
+                <h3>Language Proficiency</h3>
+              </div>
+              <div class="sub-content">
+                <ul class="list-inline">
+                  <li>
+                    <h5>English</h5>
+                    <ul>
+                      <li><i class="fa fa-star" aria-hidden="true"></i></li>
+                      <li><i class="fa fa-star" aria-hidden="true"></i></li>
+                      <li><i class="fa fa-star" aria-hidden="true"></i></li>
+                      <li><i class="fa fa-star" aria-hidden="true"></i></li>
+                      <li><i class="fa fa-star" aria-hidden="true"></i></li>
+                    </ul>
+                  </li>
+                  <li>
+                    <h5>Hindi</h5>
+                    <ul>
+                      <li><i class="fa fa-star" aria-hidden="true"></i></li>
+                      <li><i class="fa fa-star" aria-hidden="true"></i></li>
+                      <li><i class="fa fa-star" aria-hidden="true"></i></li>
+                      <li><i class="fa fa-star" aria-hidden="true"></i></li>
+                      <li><i class="fa fa-star" aria-hidden="true"></i></li>
+                    </ul>
+                  </li>
+                </ul>
+              </div>
+            </div>
+            <hr>
+            <div class="expertise">
+              <div class="title">
+                <div class="icons">
+                  <i class="fa fa-thumbs-o-up" aria-hidden="true"></i>
+                </div>
+                <h3>Skills</h3>
+              </div>
+              <div class="sub-content">
+                <div class="row">
+                  <div class="col-md-4">
+                    <div class="rating-bar">
+                      <label>Java</label>
+                      <div class="skill-progress">
+                        <div class="progress">
+                          <div class="progress-bar" role="progressbar" aria-valuenow="60" aria-valuemin="0"
+                            aria-valuemax="100">
+                          </div>
+                        </div>
+                      </div>
+                      <label>WordPress</label>
+                      <div class="skill-progress">
+                        <div class="progress">
+                          <div class="progress-bar" role="progressbar" aria-valuenow="90" aria-valuemin="0"
+                            aria-valuemax="100">
+                          </div>
+                        </div>
+                      </div>
+                      <label>Python(Machine Learning,Jupyter Notebook)</label>
+                      <div class="skill-progress">
+                        <div class="progress">
+                          <div class="progress-bar" role="progressbar" aria-valuenow="90" aria-valuemin="0" aria-valuemax="100">
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                  <div class="col-md-4">
+                    <div class="skill rating-bar">
+                      <label>C & C++</label>
+                      <div class="skill-progress">
+                        <div class="progress">
+                          <div class="progress-bar" role="progressbar" aria-valuenow="60" aria-valuemin="0"
+                            aria-valuemax="100">
+                          </div>
+                        </div>
+                      </div>
+                      <label>Android DevlopMent</label>
+                      <div class="skill-progress">
+                        <div class="progress">
+                          <div class="progress-bar" role="progressbar" aria-valuenow="60" aria-valuemin="0"
+                            aria-valuemax="100">
+                          </div>
+                        </div>
+                      </div>
+                      <label>DBMS(Database Management System)</label>
+                      <div class="skill-progress">
+                        <div class="progress">
+                          <div class="progress-bar" role="progressbar" aria-valuenow="90" aria-valuemin="0" aria-valuemax="100">
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                  <div class="col-md-4">
+                    <div class="skill rating-bar">
+                      <label>Html 5 & CSS 3</label>
+                      <div class="skill-progress">
+                        <div class="progress">
+                          <div class="progress-bar" role="progressbar" aria-valuenow="95" aria-valuemin="0"
+                            aria-valuemax="100">
+                          </div>
+                        </div>
+                      </div>
+                      <label>PHP</label>
+                      <div class="skill-progress">
+                        <div class="progress">
+                          <div class="progress-bar" role="progressbar" aria-valuenow="70" aria-valuemin="0"
+                            aria-valuemax="100">
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <hr>
+            <div class="personal-info">
+              <div class="title">
+                <div class="icons">
+                  <i class="fa fa-user-secret" aria-hidden="true"></i>
+                </div>
+                <h3>Personal Info</h3>
+              </div>
+              <div class="sub-content">
+                <ul class="address">
+                  <li>
+                    <h5>Full Name </h5> <span>:</span>Abhishek Garg
+                  </li>
+                  <li>
+                    <h5>Father's Name </h5> <span>:</span>Adesh Kumar Garg
+                  </li>
+                  <li>
+                    <h5>Mother's Name </h5> <span>:</span>Mukta Garg
+                  </li>
+                  <li>
+                    <h5>Date of Birth </h5> <span>:</span>05/07/1997
+                  </li>
+                  <li>
+                    <h5>Birth Place </h5> <span>:</span>GHAZIABAD
+                  </li>
+                  <li>
+                    <h5>Nationality </h5> <span>:</span>Indian
+                  </li>
+                  <li>
+                    <h5>Sex </h5> <span>:</span>Male
+                  </li>
+                </ul>
+              </div>
+            </div>
+            <hr>
+
+            <div class="declaration">
+              <div class="title">
+                <div class="icons">
+                  <i class="fa fa-hand-peace-o" aria-hidden="true"></i>
+                </div>
+                <h3>Declaration</h3>
+              </div>
+              <div class="sub-content">
+                <p>I do hereby declare that the above particulars of facts and information stated are true, correct and
+                  complete to the
+                  best of my belief and knowledge </p>
+                <div class="signature">
+                  <h1>Abhishek Garg</h1>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="button">
+            <a href="Resume.pdf" class="btn btn-primary">Download My Resume as a .pdf file</a>
+          </div>
+          <div id="vc-feelback-main" data-access-token="d9b3fa0bfb134f3e8e4128f40b1e2ff4" data-display-type="4"></div>
+        </div>
+      </div>
+    </div>
+  
